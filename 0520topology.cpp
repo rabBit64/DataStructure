090174aefc058b29c72eca605eda1f16600036ef
Anna Kim <48214314+rabBit64@users.noreply.github.com>
2019-05-20 22:44:14 +0900

    Update 0520topology.cpp

--- 0520topology.cpp
+++ 0520topology.cpp
@@ -1,3 +1,4 @@
+/*위상정렬(Topological Sort)란 싸이클 없는 방향그래프에서 정점을 선형순서로 나열하는 것 */
 #include <iostream>
 #include <queue>
 #include <vector>
@@ -33,12 +34,14 @@ void Topology::topologySorting() {
 	}
 	//위상정렬이 완전히 수행하려면 정확하게 N개의 정점을 방문한다.
 	for (unsigned i = 0; i < vtx; i++) {
+		//N개를 방문하기 전에 큐가 빈다면 사이클이 발생할 것이다.
 		if (q.empty()) {
 			cout << "사이클이 발생하였습니다."; return;
 		}
 		int x = q.front(); q.pop();
-		result[i] = x;
+		result[i] = x; //순서가 되는 정점 입력
 		for (unsigned i = 0; i < Edges[x].size(); i++) {
+		//새롭게 진입차수를 재구성하고 진입차수가 0인 정점을 큐에 
 			int y = Edges[x][i];
 			if (--inDegree[y] == 0) q.push(y);
 		}
